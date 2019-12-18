5cbdfa7179e2be1c23dc0957b0dec700357b32c6
Neuring <36540897+Recursived@users.noreply.github.com>
2019-12-18 20:15:54 +0100

    Update ocrapi.py

--- ocrAPI/ocrapi.py
+++ ocrAPI/ocrapi.py
@@ -6,7 +6,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Transform an image into text (preview for a ocr web api)")
-    parser.add_argument("image", type=str, help="lien vers une image")
+    parser.add_argument("image", type=str, help="link to an image")
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     args = parser.parse_args()
     sys.stdout.write(pytesseract.image_to_string(Image.open(args.image)))
