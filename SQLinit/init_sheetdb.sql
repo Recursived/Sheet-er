22d3211fe67c2e1cf2aa50a3a34f49d6a3a5a96d
Alexandre MANETA <alex.m94430@gmail.com>
2020-09-26 19:37:45 +0200

    MAJ docker-compose & init sql

--- SQLinit/init_sheetdb.sql
+++ SQLinit/init_sheetdb.sql
@@ -1,2 +1,7 @@
+/* Creation de la bdd pour l'api REST 'Sheet' */
 CREATE DATABASE sheet_db;
 GRANT ALL PRIVILEGES ON DATABASE sheet_db TO postgres;
+
+/* Creation de la bdd pour l'api REST 'User' */
+CREATE DATABASE user_db;
+GRANT ALL PRIVILEGES ON DATABASE user_db TO postgres;
