d6a3c4152d0b4b10a8c0461ba94081e7bb3917ea
Alexandre MANETA <alex.m94430@gmail.com>
2020-10-04 01:06:52 +0200

    Ajout field id dans serializers

--- sheetAPI/api/serializers.py
+++ sheetAPI/api/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
-from .models import Sheet, SheetType, SheetTag
+
+from .models import Sheet, SheetTag, SheetType
+
 
 class SheetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sheet
-        fields = [ 'content', 'title',  'subject', 'mark', 'tags', 'creation_date', 'plagiarism_rate']
+        fields = ['content', 'title',  'subject', 'mark',
+                  'tags', 'creation_date', 'plagiarism_rate']
 
 
 class SheetTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SheetType
-        fields = ['label']
+        fields = ['id', 'label']
+
 
 class SheetTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = SheetTag
-        fields = ['label']
+        fields = ['id', 'label']
