e7f8732715b0067327c0427e53fcf044a9752aa8
Alexandre MANETA <alex.m94430@gmail.com>
2020-10-27 22:04:30 +0100

    Fin écriture tests pour userAPI

--- userAPI/api/tests.py
+++ userAPI/api/tests.py
@@ -2,52 +2,22 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import SheeterUser
-from .serializers import SheeterUserSerializer
+from django.contrib.auth.models import User
+from social_django.models import UserSocialAuth
+
 
 
 class SheeterUserTest(APITestCase):
     def setUp(self):
-        self.post_data = {
-            'user_id': '12312341',
-            'name': 'Alexandre MANETA',
-            'email': 'test1@email.com',
-            'profile_pic': 'https://github.com/',
-            'account_type': 'Fb'
-        }
-
-        self.data = {
-            'user_id': '12312321',
-            'name': 'Kevin Tran',
-            'email': 'test@email.com',
-            'profile_pic': 'https://github.com/',
-            'account_type': 'Fb'
-        }
-
-        self.user = SheeterUser.objects.create(**self.data)
-        self.json_data = SheeterUserSerializer(self.user).data
-        self.json_data.update({"name": "Alexandre MANETA"})
-
-    def test_can_create_user(self):
-        response = self.client.post(
-            reverse('sheeteruser-list'), self.post_data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_can_read_user_list(self):
-        response = self.client.get(reverse('sheeteruser-list'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_can_read_user_detail(self):
+        self.user = User.objects.create(username="alex", password="123321")
+        self.social_user = UserSocialAuth.objects.create(
+            user = self.user,
+            provider = "facebook",
+            uid=3779421658752048
+        )
+    
+    def test_user_existence(self):
         response = self.client.get(
-            reverse('sheeteruser-detail', args=[self.user.user_id]))
+            f"/user/{self.social_user.uid}/{self.social_user.provider}"
+            )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_can_update_user(self):
-        response = self.client.put(
-            reverse('sheeteruser-detail', args=[self.user.user_id]), self.json_data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_can_delete_user(self):
-        response = self.client.delete(
-            reverse('sheeteruser-detail', args=[self.user.user_id]))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- userAPI/api/urls.py
+++ userAPI/api/urls.py
@@ -15,6 +15,6 @@ router.get_api_root_view().cls.__doc__ = "Api endpoint for elements related to u
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    path('user/<uid>/<provider>', UserView.as_view()) # uid --> id unique + provider --> facebook, google
+    path('user/<uid>/<provider>', UserView.as_view(), name="user_exist") # uid --> id unique + provider --> facebook, google
     # url(r'^', include(router.urls))
 ]
