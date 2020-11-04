49d74fb7966283e2ae7eb993194f80fe70fb897c
Alexandre MANETA <alex.m94430@gmail.com>
2020-11-04 22:27:45 +0100

    Correctif API : vérification existence 'User'

--- userAPI/api/serializers.py
+++ userAPI/api/serializers.py
@@ -1,19 +1,28 @@
 from django.contrib.auth.models import User
+from django.db.models import fields
 from rest_framework import serializers
 from social_django.models import UserSocialAuth
+from oauth2_provider.models import RefreshToken, AccessToken
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name',
-                  'last_name', 'email', 'date_joined']
+                  'last_name', 'email', 'date_joined', 'last_login']
 
 
-class UserSocialAuthSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+class AccessTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccessToken
+        fields = ['token', 'expires', 'scope']
+
+
+class RefreshTokenSerializer(serializers.ModelSerializer):
+    access_token = AccessTokenSerializer(read_only=True)
+    user = UserSerializer(read_only=True) 
 
     class Meta:
-        model = UserSocialAuth
-        fields = ['uid', 'user', 'provider',
-                  'created', 'modified', "extra_data"]
+        model = RefreshToken
+        fields = ['token', 'user', 'access_token',
+                  'created', 'updated', 'revoked']
--- userAPI/api/views.py
+++ userAPI/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from social_django.models import UserSocialAuth
+from oauth2_provider.models import RefreshToken
 
 from .mixins import MultipleFieldLookupMixin
-from .serializers import UserSocialAuthSerializer
+from .serializers import RefreshTokenSerializer
 
 
 class UserView(MultipleFieldLookupMixin, RetrieveAPIView):
@@ -11,11 +12,12 @@ class UserView(MultipleFieldLookupMixin, RetrieveAPIView):
     A endpoint to get information on a user according to his uid
     """
     queryset = UserSocialAuth.objects.all()
-    serializer_class = UserSocialAuthSerializer
+    serializer_class = RefreshTokenSerializer
     lookup_fields = ("uid", "provider")
 
     def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
+        social_user_instance = self.get_object()
+        rf_instance = RefreshToken.objects.get(user=social_user_instance.user)
+        serializer = self.get_serializer(rf_instance)
         return Response(serializer.data)
 
