954a66d1f1e9cf3713d78b1904352de5d389a47f
Alexandre MANETA <alex.m94430@gmail.com>
2020-11-09 22:42:44 +0100

    Ajout url du serveur

--- userAPI/userapi/urls.py
+++ userAPI/userapi/urls.py
@@ -13,6 +13,7 @@ schema_view = get_schema_view(
       title=API_TITLE,
       default_version='v1',
       description=API_DESCRIPTION,
+      url="http://localhost:8001",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="test@email.com"),
       license=openapi.License(name="MIT License"),
