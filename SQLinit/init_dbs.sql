73320f7599d96db67ea409ccdeaebf7b6426a4c3
Alexandre MANETA <alex.m94430@gmail.com>
2020-11-20 12:15:22 +0100

    Création de l'api de "contact us"

--- SQLinit/init_dbs.sql
+++ SQLinit/init_dbs.sql
@@ -8,4 +8,4 @@ GRANT ALL PRIVILEGES ON DATABASE user_db TO postgres;
 
 /* Creation de la bdd pour l'api REST 'Message' */
 CREATE DATABASE contactus_db;
-GRANT ALL PRIVILEGES on DATABASE contactus_db TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE contactus_db TO postgres;
---
+++ contactusAPI/api/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+# from .views import SheetTypeViewSet, SheetTagViewSet, SheetViewSet
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+
+# We change the name of the front page of the API
+router.get_api_root_view().cls.__name__ = "Contact us Api endpoint"
+router.get_api_root_view().cls.__doc__ = "Api endpoint for elements related to messaging"
+
+
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
