d923b39e7bdd3ddfe22becd0d9898c63ad613a52
Alexandre MANETA <alex.m94430@gmail.com>
2020-11-25 22:38:25 +0100

    Ajout contact us api

--- sheeter_user_interface/app/utils/api.js
+++ sheeter_user_interface/app/utils/api.js
@@ -6,15 +6,24 @@ export const CLIENT_ID = "GksdhKpX9wvGPiRc6TQv2KdV95Gy5kTL1bICpiME";
 // Definition of the const to retrieve the corresponding API
 export const RETRIEVE_SHEETAPI = "sheet";
 export const RETRIEVE_USERAPI = "user";
+export const RETRIEVE_CONTACTUS = "contact_us";
 
 const userAPI = new OpenAPIClientAxios({ 
     definition : "http://localhost:8001/swagger.json", 
     validate: false,
     withServer: { url: 'http://localhost:8001', description: 'Auth server' }
 });
+
 const sheetAPI =  new OpenAPIClientAxios({ 
     definition : "http://localhost:8000/swagger.json", 
     validate: false,
+    withServer: { url: 'http://localhost:8000', description: 'Sheet server' }
+});
+
+const contactusAPI =  new OpenAPIClientAxios({ 
+    definition : "http://localhost:8002/swagger.json", 
+    validate: false,
+    withServer: { url: 'http://localhost:8000', description: 'Contact server' }
 });
 
 
@@ -29,6 +38,8 @@ export default function getAPI(name){
             return userAPI;
         case RETRIEVE_SHEETAPI:
             return sheetAPI;
+        case RETRIEVE_CONTACTUS:
+            return contactusAPI;
         default:
             return null;
     }
