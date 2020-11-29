7c55b1747d72c395871186dc365daee05ffcb848
Alexandre MANETA <alex.m94430@gmail.com>
2020-11-29 16:39:50 +0100

    Ajout model 'Response' dans admin panel

--- contactusAPI/api/admin.py
+++ contactusAPI/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Message
+from .models import Category, Message, Response
 
 # Register your models here.
 admin.site.register(Message)
 admin.site.register(Category)
+admin.site.register(Response)
