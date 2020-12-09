2e723995e9bcb44c14b4bfd1c61558ab571d49e7
Alexandre MANETA <alex.m94430@gmail.com>
2020-12-09 14:44:04 +0100

    Correction bug refresh_token

--- sheeter_user_interface/app/configureStore.js
+++ sheeter_user_interface/app/configureStore.js
@@ -11,8 +11,12 @@ import { FormattedMessage } from 'react-intl';
 import { enqueueSnackbar, closeSnackbar } from 'containers/NotifProvider/actions';
 import { isRefreshAction } from 'containers/App/actions';
 import messages from 'containers/App/messages';
+
+// Misc import
 import createReducer from './reducers';
-import { CLIENT_ID } from 'utils/api';
+import { isLoggedSuccessAction } from 'containers/App/actions';
+import getApi from 'utils/api';
+import { RETRIEVE_USERAPI, CLIENT_ID } from 'utils/api';
 
 export default function configureStore(initialState = {}, history) {
   let composeEnhancers = compose;
@@ -37,41 +41,48 @@ export default function configureStore(initialState = {}, history) {
   const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
 
   const checkTokenExpirationMiddleware = store => next => action => {
-    const user_info = store.getState().global.user_info;
-    const dispatch = store.dispatch;
-    if (user_info != null){
+    const global_scope = store.getState().global;
+    const user_info = global_scope.user_info,
+      isLogged = global_scope.loggedIn,
+      conn_info = global_scope.conn_info,
+      dispatch = store.dispatch;
+
+
+    // User shoud be logged to refresh token
+    if (isLogged && user_info !== null) {
       const expiry_date = new Date(user_info.access_token.expires);
       const today = new Date();
-     
-      
 
-      if (today > expiry_date){
-        const axios = require('axios');
-        const qs = require('qs');
-        let data = qs.stringify({
-          grant_type : 'refresh_token',
-          client_id : CLIENT_ID,
-          refresh_token: user_info.token,
-        });
-        let config = { 
-          method: 'post',
-          url: 'http://localhost:8001/auth/token',
-          headers: { 
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          data : data
-        };
-        axios(config)
-        .then(res => dispatch(isRefreshAction(res)))
-        .catch(error => (
+      if (today > expiry_date) {
+        try {
+          const api = getApi(RETRIEVE_USERAPI);
+          await api.init();
+
+          const client = await api.getClient();
+          const retour = await client.paths["/auth/convert-token/"].post({
+            grant_type: 'convert_token',
+            client_id: CLIENT_ID,
+            backend: conn_info.backend,
+            token: conn_info.social_token
+          });
+
+          const response = await client.paths["/user/{uid}/{provider}"].get(
+            { uid: conn_info.uid, provider: conn_info.backend },
+            null,
+            { headers: { 'Authorization': `Bearer ${retour.data.access_token}` } }
+          );
+
+          // We inform the global store about the update
+          dispatch(isLoggedSuccessAction(response.data));
+        } catch (error) {
           dispatch((enqueueSnackbar({
             message: <FormattedMessage {...messages.problemRefreshToken} />,
             options: {
-                key: new Date().getTime() + Math.random(),
-                variant: 'error'
-            },
-          })))
-        ));
+              key: new Date().getTime() + Math.random(),
+              variant: 'error'
+            }
+          })));
+        }
       }
     }
     next(action);
@@ -82,7 +93,7 @@ export default function configureStore(initialState = {}, history) {
   // 2. routerMiddleware: Syncs the location/URL path to the state
   const middlewares = [
     checkTokenExpirationMiddleware,
-    sagaMiddleware, 
+    sagaMiddleware,
     routerMiddleware(history)
   ];
 
