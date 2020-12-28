487de0fef04f783018eaff7cda5efaac6c0a8cc9
Alexandre MANETA <alex.m94430@gmail.com>
2020-12-28 23:37:30 +0100

    Fin visuel de l'éditeur

--- sheeter_user_interface/app/components/SheeterEditor/WrapperEditor.js
+++ sheeter_user_interface/app/components/SheeterEditor/WrapperEditor.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
 
-export const WrapperEditor = (theme) => styled.div`
 
+export const WrapperEditor = styled.div`
+    background-color: ${props => props.theme.palette.background.paper}; 
+    box-shadow: ${props => props.theme.shadows[15]};
+    border: ${props => props.focus ? "2px" : "1px"} solid ${props => props.theme.palette.primary.main};
+    min-height: 50vh;
+    cursor: text;
 
+    & > .DraftEditor-root  {
+        min-height: inherit !important;
+    }
+
+    & > * > .DraftEditor-editorContainer {
+        min-height: inherit !important;
+    }
+    
+    & > * > * > .public-DraftEditor-content {
+        min-height: inherit !important;
+        padding: ${props => props.theme.spacing(1)}px;
+    }
 `
