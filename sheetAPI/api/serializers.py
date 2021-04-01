1a48d0e962623b6a58abfa597479bea3d7411f94
Alexandre MANETA <alex.m94430@gmail.com>
2021-04-01 15:02:08 +0200

    Suppr migrations dans folder + docker-compose

--- sheetAPI/api/serializers.py
+++ sheetAPI/api/serializers.py
@@ -3,11 +3,7 @@ from rest_framework import serializers
 from .models import Sheet, SheetTag, SheetType
 
 
-class SheetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sheet
-        fields = ['id', 'content', 'title',  'subject', 'mark',
-                  'tags', 'creation_date', 'plagiarism_rate', 'locale']
+
 
 
 class SheetTypeSerializer(serializers.ModelSerializer):
@@ -20,3 +16,12 @@ class SheetTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = SheetTag
         fields = ['id', 'label']
+
+
+class SheetSerializer(serializers.ModelSerializer):
+    tags = SheetTagSerializer(read_only=True)
+    subject = SheetTypeSerializer(read_only=True)
+    class Meta:
+        model = Sheet
+        fields = ['id', 'content', 'descr' , 'title',  'subject', 'mark',
+                  'tags', 'creation_date', 'plagiarism_rate', 'locale']
